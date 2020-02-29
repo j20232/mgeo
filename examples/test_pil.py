c72d8fad26a4101277cbec0b08cf9f26ce0f3877
mocobt <mocobt@gmail.com>
2020-03-01 03:01:22 +0900

    add show_histgrams

--- examples/test_pil.py
+++ examples/test_pil.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     img = transform.rotate(img, 10)
     img = transform.resize(img, (512, 512))
     points = np.array([[100, 200], [100, 500], [400, 200], [400, 500]])
-    mgeo.utils.visualize.show(img, points=points, to_gray=True)
+    mgeo.utils.visualize.show(img, points=points)
     mgeo.utils.visualize.show_histgram(img[:, :, 0])
--- mgeo/utils/visualize.py
+++ mgeo/utils/visualize.py
@@ -1,10 +1,8 @@
 import matplotlib.pyplot as plt
 
 
-def show(np_img, points=None, mark="b*", size=(6, 6), to_gray=False, show_axis=False):
+def show(img, points=None, mark="b*", size=(6, 6), cmap="viridis", show_axis=False):
     plt.figure(figsize=size)
-    if to_gray:
-        plt.gray()
     if not show_axis:
         plt.axis("off")
     if points is not None and points.ndim == 2:
@@ -12,18 +10,31 @@ def show(np_img, points=None, mark="b*", size=(6, 6), to_gray=False, show_axis=F
             plt.plot(points[:, 0], points[:, 1], mark)
         else:
             plt.plot(points[0], points[1], mark)
-    plt.imshow(np_img)
+    plt.imshow(img, cmap=cmap)
     plt.show()
 
 
-def show_histgram(np_img, size=(15, 6), bins=128, show_axis=False):
-    assert np_img.ndim == 2
+def show_imgs(img_list, title_list=None, cmap="viridis", size=(16, 8), show_axis=False):
+    if title_list is not None:
+        assert len(img_list) == len(title_list)
     plt.figure(figsize=size)
-    plt.gray()
+    for idx, img in enumerate(img_list):
+        plt.subplot(1, len(img_list), idx + 1)
+        plt.imshow(img, cmap=cmap)
+        if title_list is not None:
+            plt.title(title_list[idx])
+        if not show_axis:
+            plt.axis("off")
+    plt.show()
+
+
+def show_histgram(img, size=(15, 6), bins=128, cmap="viridis", show_axis=False):
+    assert img.ndim == 2
+    plt.figure(figsize=size)
 
     # Original
     plt.subplot(1, 3, 1)
-    plt.imshow(np_img)
+    plt.imshow(img, cmap=cmap)
     plt.title("Original")
     plt.axis("equal")
     if not show_axis:
@@ -31,7 +42,7 @@ def show_histgram(np_img, size=(15, 6), bins=128, show_axis=False):
 
     # Contour
     plt.subplot(1, 3, 2)
-    plt.contour(np_img, origin="image")
+    plt.contour(img, origin="image", cmap=cmap)
     plt.title("Contour")
     plt.axis("equal")
     if not show_axis:
@@ -39,22 +50,19 @@ def show_histgram(np_img, size=(15, 6), bins=128, show_axis=False):
 
     # Histgram
     plt.subplot(1, 3, 3)
-    plt.hist(np_img.flatten(), bins)
+    plt.hist(img.flatten(), bins)
     plt.title("Histgram")
     plt.show()
 
 
-def show_imgs(img_list, title_list=None, size=(16, 8), to_gray=False, show_axis=False):
+def show_histgrams(img_list, title_list=None, bins=256, cmap="viridis", size=(16, 8)):
     if title_list is not None:
         assert len(img_list) == len(title_list)
     plt.figure(figsize=size)
-    if to_gray:
-        plt.gray()
     for idx, img in enumerate(img_list):
         plt.subplot(1, len(img_list), idx + 1)
-        plt.imshow(img)
+        plt.hist(img.flatten(), bins)
         if title_list is not None:
             plt.title(title_list[idx])
-        if not show_axis:
-            plt.axis("off")
+        plt.xlim(0, bins)
     plt.show()
